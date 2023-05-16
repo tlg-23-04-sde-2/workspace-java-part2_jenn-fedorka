f27d0c8d0719d34b43f3125e1bd88e2bc2943cd9
Jennifer Fedorka <Jennifer.fedorka@tlgcohort.com>
2023-05-16 14:33:15 -0500

    Lab03.1 Complete, including @before, @after

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,13 +9,52 @@
 package com.math;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
+
+import org.junit.*;
 
 public class CalculatorTest {
 
-  @Test
-  public void testAdd() {
-    Calculator calc = new Calculator();
-    assertEquals(5, calc.add(1, 4));  // expected, actual
-  }
+    @BeforeClass
+    public static void initializeEntireTestRun() {
+        System.out.println("Initial");
+    }
+
+    @AfterClass
+    public static void finalizeEntireTestRun() {
+        System.out.println("Finalize");
+    }
+
+    //business objects under test AKA fixture
+    private Calculator calc;
+
+    @Before
+    public void setUp() {
+        System.out.println("setUp");
+        calc = new Calculator();
+    }
+
+    @After
+    public void cleanUp() {
+        System.out.println("cleanUp");
+    }
+
+    @Test
+    public void testIsEven() {
+        System.out.println("testIsEven");
+        assertTrue(calc.isEven(10));
+        assertFalse(calc.isEven(11));
+    }
+
+    @Test
+    public void testDivide() {
+        System.out.println("testDivide");
+        assertEquals(2.5, calc.divide(5,2), 0.001); //expected, actual, delta
+    }
+
+    @Test
+    public void testAdd() {
+        System.out.println("testAdd");
+        assertEquals(5, calc.add(1, 4));  // expected, actual
+    }
+
 }
