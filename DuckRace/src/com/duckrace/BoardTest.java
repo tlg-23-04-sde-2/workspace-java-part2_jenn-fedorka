f9cfe618e5fc43df2685c5db84bfe35d21f4785d
Jennifer Fedorka <Jennifer.fedorka@tlgcohort.com>
2023-05-19 12:39:07 -0500

    DuckRace Second Iteration (load student ids into Student idMap)

---
+++ DuckRace/src/com/duckrace/BoardTest.java
@@ -0,0 +1,10 @@
+package com.duckrace;
+
+class BoardTest {
+
+    public static void main(String[] args) {
+        //create a board object, which initialize its field
+        Board board = new Board();
+        board.dumpStudentIdeaMap();
+    }
+}
