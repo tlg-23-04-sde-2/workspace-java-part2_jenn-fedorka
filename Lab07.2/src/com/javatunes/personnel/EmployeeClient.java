9e25956ce4bd45919579b3c433988b5dce7a7909
Jennifer Fedorka <Jennifer.fedorka@tlgcohort.com>
2023-05-22 17:19:01 -0500

    Lab07.2 complete - custon annotation @fast

---
+++ Lab07.2/src/com/javatunes/personnel/EmployeeClient.java
@@ -0,0 +1,9 @@
+package com.javatunes.personnel;
+
+class EmployeeClient {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee();
+        emp1.pay();
+        emp1.work();
+    }
+}
